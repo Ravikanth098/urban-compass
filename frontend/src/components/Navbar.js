391be2ab0982789475a61a599cf8cccb9889fb93
Ravikanth098 <24282243@student.ncirl.ie>
2026-04-02 17:43:37 +0100

    cicd

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,12 +4,12 @@ import "../styles/navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">Smart city</div>
+      <div className="logo">SmartCity Application</div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/services">Services</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li> <Link to="/contact">Conact Us</Link></li>
+        <li> <Link to="/contact">Contact Us</Link></li>
       </ul>
     </nav>
   );
